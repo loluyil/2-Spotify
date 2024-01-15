7ab9afa41f23d4941a3d601974cb41d4c9a0a071
loluyil <twitchluyil@gmail.com>
2024-01-15 17:13:17 +1300

    nav-card
    finished with the nav-card history buttons. need to adjust the fa-plus button in order to be center, and fix the hover properly to where the container will highlight the icon also.

--- spotify-app/src/app/components/nav-card/nav-card.component.ts
+++ spotify-app/src/app/components/nav-card/nav-card.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+//icons
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav-card',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './nav-card.component.html',
   styleUrl: './nav-card.component.css'
 })
 export class NavCardComponent {
-
+  fachevronLeft = faChevronLeft;
+  fachevronRight = faChevronRight;
 }
